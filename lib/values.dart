cb3e6d475980483c7bef44074a43bde7f3c92c1a
David Batista <dev.davidbatista@gmail.com>
2022-11-30 13:51:04 +0000

    Final presentation code

--- lib/values.dart
+++ lib/values.dart
@@ -122,6 +122,7 @@ List<Widget> slides = [
       author: author,
       page: 5
   ),
+  /** CHANGE OF TOPIC TO DART Syntax */
   TitleAndSub(
     title: const Text("Syntax",
       style: TextStyle(
@@ -145,7 +146,34 @@ List<Widget> slides = [
     page: 7,
     enabled: true
   ),
-  LiveDemo(author: author, page: 8),
+  TitleAndCode(
+      title: const Text("Functions,\nFlow Control \n& Loops",
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 96
+        ),
+      ),
+      subtitle: Text(""),
+      code: controlCode,
+      author: author,
+      page: 8,
+      enabled: true
+  ),
+  TitleAndCode(
+      title: const Text("More Dart Syntax",
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 96
+        ),
+      ),
+      subtitle: Text(""),
+      code: otherCode,
+      author: author,
+      page: 9,
+      enabled: true
+  ),
+  /** LIVE DEMO */
+  LiveDemo(author: author, page: 10),
 ];
 
 
@@ -297,7 +325,11 @@ String syntaxComparison = '''
     Const pode ser omitido
  * Final
     Apesar de um objeto declarado como final não poder ser modificado, os seus campos, a não ser que também sejam final, podem ser modificados. Isto difere do const, onde nem o objeto nem os seus campos podem ser alterados, const é completamente immutável.
- *  */
+ *
+ * Data Types
+    Dart suporta alguns tipos de dados nativamente, estes como já vimos são objetos.
+    Tipos de dados suportados nativamente: Numbers (int, double), Strings (String), Booleans (bool), Lists (List, also known as arrays), Sets (Set), Maps (Map), Symbols (Symbol), The value null (Null)
+ * */
 String variables = '''
 //Variable Declaration
   String name = 'Bob';
@@ -338,5 +370,211 @@ String variables = '''
 //Final
   final Person person = Person(firstName: "Bob", lastName:"Bobby")
   person.lastName = "Robby" // Works fine!
+  
+//Native Data Types
+  //Numbers and operations
+  int a = 10;
+  final static double pi = 3.1415;
+  var b = a * pi;
+  num d = b % 7;
+  //Numbers(int,double,num)
+  //Strings (String)
+  //Booleans (bool)
+  //Lists (List, also known as arrays)
+  //Sets (Set)
+  //Maps (Map)
+  //Symbols (Symbol)
+  //The value null (Null)
 ''';
 
+/**
+ *
+ * Functions
+    Functions are pretty similar to any other language, the return type, or the argument type, can both be inferred as you see bellow. Both those functions work the same.
+    É possivel declarar uma funcão de forma curta com shorthand syntax. A expressão "=> x" é a versão simples de "{ return x; }".
+ * Named parameters
+    Através da syntax {param1, param2}, é possivel dar parametros com nome, que têm que ser chamados depois atravez de func(param1: value).
+    É possivel dar valores default aos named parameters, se for atribuido o valor default na declaração da função.
+ * The main function
+    The is always the entry point to the dart program.
+ * first-class Functions
+    All functions are first-class objects, that allows them to be passed to other functions as an object, in this case, a function that prints a number, is passed to the .forEach function
+ * Flow Control
+    If, são como em qualquer outra linguagem, pode ser posto com um else, ou em cadeia com if else.
+    é possivel fazer certas operações condicionais em one liner através da suyntax bool ? expr1 : expr2
+    E switches também existem como em qualquer outra linguagem
+ * Loops
+    Em loops temo o for, também parecido com qualquer outra linguagem, neste exemplo, vamos criamos um array vaziu, e para os dois ciclos do for, adicionamos ao array uma funcção que da print de um numero. De seguida, através do for, percorremos o array, e executamos cada função do array.
+    Como vimos ali em cima, for loops são já uma função do objeto lista, ou array. Onde pode receber uma função, que vai executar para cada elemento da lista.
+    Também em loops temos o while e do while.
+ *  */
+String controlCode = '''
+//Functions
+  //Normal declaration with type anotation
+    num addNum(num a, num b){
+      return a + b;
+    }
+  
+  //Inferred types
+    addNum2(a,b){
+      return a + b;
+    }
+ 
+  //Shorthand syntax
+    num addNum(num a, num b) => a + b;
+    
+  //Named Parameters
+    void enableFlags({bool? bold, bool? hidden}) {...}
+    ...
+    enableFlags(bold: true, hidden: false);
+  
+    // Sets the [bold] and [hidden] flags ...
+    void enableFlags({bool bold = false, bool hidden = false}) {...}
+    
+    // bold will be true; hidden will be false.
+    enableFlags(bold: true);
+    
+  // The Main Function
+    void main() {
+      print('Hello, World!');
+    }
+  
+  //Functions as first-class objects
+    void printElement(int element) {
+      print(element);
+    }
+    
+    var list = [1, 2, 3];
+    
+    // Pass printElement as a parameter.
+    list.forEach(printElement);
+    
+//Flow control
+  //if
+    if (isRaining()) {
+      you.bringRainCoat();
+    } else if (isSnowing()) {
+      you.wearJacket();
+    } else {
+      car.putTopDown();
+    }
+    
+  //Conditional expressions
+    var visibility = isPublic ? 'public' : 'private';
+    
+  //switch
+    var command = 'OPEN';
+    switch (command) {
+      case 'CLOSED':
+        executeClosed();
+        break;
+      case 'PENDING':
+        executePending();
+        break;
+      case 'APPROVED':
+        executeApproved();
+        break;
+      case 'DENIED':
+        executeDenied();
+        break;
+      case 'OPEN':
+        executeOpen();
+        break;
+      default:
+        executeUnknown();
+    }
+
+//Loops
+  //For
+    var callbacks = [];
+    for (var i = 0; i < 2; i++) {
+      callbacks.add(() => print(i));
+    }
+    
+    for (final c in callbacks) {
+      c();
+    }
+  //forEach()
+    var collection = [1, 2, 3];
+    collection.forEach(print); // 1 2 3
+    
+  //while
+    while (!isDone()) {
+      doSomething();
+    }
+  
+  //do..while
+    do {
+      printLine();
+    } while (!atEndOfPage());
+
+''';
+
+/**
+ * Other syntax
+ * In this section vamos ver rápidamente outras syntaxs menos relevantes, mas que podem ser interessantes
+ * Async
+    Podemos trabalhar asyncronamente atravez de objetos Future, e as keywords async e await.
+    Isto é parecido com muitas outras linguagems, onde Future são as promessas, que podem ser resolvidas através de await. E qualquer código async tem que estar numa função declarada como async
+ * Streams
+    Quando é expectado receber um conjunto de dados continuos async, isto pode ser usado por exemplo para receber uma stream de um tipo de dados de um ervidor, pode se usar a seguinte syntax para receber a stream de dados.
+    Neste caso o await vai esperar até a stream acabar, e para cada chunck de dados, executa algum código determinado.
+    É de notar, que Streams não são supostas usar com UI event listeners, porque enviam streams sem fim, e o await for, vai esperar até á stream acabar, o que vai ser nunca até a aplicação fechar.
+ *  Try catch
+    Como em muitas linguagem, tambem temos acesso a blocos try catch, onde codigo que pode lançar exceçoes pode ser contido num bloco try, e se realmente houver alguma excecçao pode ser tratada no bloco catch.
+ * Cascade notation
+      É possivel usar esta notção para fazer operações continuas no mesmo objeto, ao seja, neste exemplo, é excusado voltar a escrever paint as vezes todas
+ * Classes
+      Como em qualquer outra linguagem object-oriented, temos classes, funcionam da mesma maneira que java por exemplo, podemos ter propriedades, construtores, e funções.
+      Conceitos de genéricos, herança, também se aplicam ás classes de flutter.
+    Fora das classes é possivel declarar tipos de dados através de typedef
+ * */
+String otherCode = '''
+//Other syntax
+  //Async
+    Future<void> checkVersion() async {
+      var version = await lookUpVersion();
+      // Do something with version
+    }
+    
+  //Streams
+  await for (varOrType identifier in stream) {
+    // Executes each time the stream emits a value.
+  }
+    
+  //try catch
+    try {
+      version = await lookUpVersion();
+    } catch (e) {
+      // React to inability to look up the version
+    }
+    
+  //Cascade notation
+    var paint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+    //Same as 
+    var paint = Paint();
+      paint.color = Colors.black;
+      paint.strokeCap = StrokeCap.round;
+      paint.strokeWidth = 5.0;
+  
+  //Classes 
+    class Point {
+      double x = 0;
+      double y = 0;
+    
+      Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+      }
+      
+      printCoordinates(String str){
+        print([\$x,\$y]);
+      }
+    }
+  //Typedef
+    typedef IntList = List<int>;
+    IntList il = [1, 2, 3];
+''';
